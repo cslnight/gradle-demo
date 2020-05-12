3cc6a82c468989b74ae81fd41bfd877d24101965
chenshouliang <shlchen@odc.ustcsoft.com>
2020-05-12 11:45:33 +0800

    add

---
+++ publish-maven/src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @program: publish-maven
+ * @description: hello world
+ * @author: chenshouliang
+ * @create: 2020-05-12 09:40
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+public class HelloWorld {
+
+    private int id;
+    private String name;
+}
